2b1fdd77e883ed3e981945f4241c452b2efa32ec
masamune@example.com <ec2-user@ip-172-31-10-154.ap-northeast-1.compute.internal>
2023-02-01 01:25:58 +0000

    write namber

---
+++ basic_arithmetic.php
@@ -0,0 +1,32 @@
+<?php
+
+$i = random_int(1,999);
+$j = random_int(1,999);
+
+$k = $i + $j;
+echo "{$i} + {$j} = {$k} \n";
+
+// -
+$k = $i - $j;
+echo "{$i} - {$j} = {$k} \n";
+
+// *
+$k = $i * $j;
+echo "{$i} * {$j} = {$k} \n";
+
+// /
+
+$k = $i / $j;
+echo "{$i} / {$j} = {$k} \n";
+
+// %
+$k = $i % $j;
+echo "{$i} % {$j} = {$k} \n";
+
+// **
+$k = $i ** 3;
+echo "{$i} ** 3 = {$k} \n";
+
+// pow
+$k = pow($i,3);
+echo "pow({$i}, 3) = {$k} \n";
