abf06901018aabf4db89bafa54215a4a943dc95f
masamune@example.com <ec2-user@ip-172-31-10-154.ap-northeast-1.compute.internal>
2023-02-02 00:24:03 +0000

    katakaita

---
+++ tn.php
@@ -0,0 +1,10 @@
+<?php
+
+$s = "Hello";
+var_dump($s);
+
+echo $s[1] , "\n";
+
+$multi_string = "あいうえお";
+var_dump($multi_string);
+echo $multi_string[0] , "\n";
---
+++ ts.php
@@ -0,0 +1,20 @@
+<?php
+
+$s = "Hello ";
+var_dump($s);
+
+echo $s[1] , "\n";
+
+$multi_string = "ああいうえお";
+var_dump($multi_string);
+echo $multi_string [0] , "\n";
+
+$num_i = 123;
+$num_s = "123";
+var_dump($num_i);
+var_dump($num_s);
+
+$double_s = "num is {$num_i} \n";
+$single_s = 'num is {$num_i} \n';
+var_dump($double_s);
+var_dump($single_s);
